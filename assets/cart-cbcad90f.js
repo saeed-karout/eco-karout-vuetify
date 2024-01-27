c6255834fb4a31c27a6ca865d00e30ec6ec73cc1
saeed-karout <saed.karot1@gmail.com>
2024-01-27 17:56:12 +0300

    adding dist

---
+++ assets/cart-cbcad90f.js
@@ -0,0 +1,1 @@
+import{aD as s}from"./index-bd89a78f.js";const r=s("cart",{state:()=>({products:[]}),getters:{},actions:{AddToCart(t){this.products.push(t)},deleteProduct(t){this.products.splice(t,1)}},persist:!0});export{r as u};
---
+++ assets/logo-4b2579e9.js
@@ -0,0 +1,1 @@
+const e="/deploying-vite-project-example/assets/logo-22570dcf.png";export{e as _};
